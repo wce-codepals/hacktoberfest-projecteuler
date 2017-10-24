10c8f3d75f84871b942eef5ae23005e6a75114b9
sillyclee <33050724+sillyclee@users.noreply.github.com>
2017-10-24 00:15:21 -0700

    Added C solution for #2

--- solutions/problem-2/solution.c
+++ solutions/problem-2/solution.c
@@ -1,1 +1,23 @@
 /Add C solution to problem-2
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+  unsigned long int a,b,c, sum;
+  a = 0;
+  b = 1;
+  c = 0;
+  sum = 0;
+  while (c < 4000000)
+  {
+      if (c % 2 == 0)
+          sum = sum + c;
+          
+      c = a + b;
+      a = b;
+      b = c;
+  }
+  printf("%lu\n",sum);
+}
