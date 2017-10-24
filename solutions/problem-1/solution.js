d5c2ce778d606cd5231e98d6adf6930eb50c2826
Intentionally Clandestine <justmepratik@gmail.com>
2017-10-24 12:45:21 +0545

    Solution Added for PROBLEM-1 in Javascript

--- solutions/problem-1/solution.js
+++ solutions/problem-1/solution.js
@@ -1,1 +1,11 @@
-//Add JavaScript solution to problem-1
+var sumOfMultiplesOf3And5 = function (upperLimit) {
+    var sum = 0;
+    for (var i = 0; i < upperLimit; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(sumOfMultiplesOf3And5(1000));
