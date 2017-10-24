668ed87c4d0492eef4a4b01e8fdb3e827be775a8
Glen Johnson <gjohnson@adobe.com>
2017-10-24 10:24:11 -0400

    add java solution for problem 004

--- solutions/problem-4/solution.java
+++ solutions/problem-4/solution.java
@@ -1,1 +1,32 @@
-//Add Java solution to problem-4
+// Author: Glen Johnson (@gj-codes)
+// Date: 10-24-2017
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Here's the highest palindrome: " + calculate());
+    }
+    
+    
+    public static int calculate() {
+        int max = 0;
+        // only checking product of 3 digit numbers
+        for (int i = 100; i < 1000; i++) {
+            for (int j = 100; j < 1000; j++) {
+                int prod = i * j;
+                if (isPalindrome(prod) && (prod > max))
+                    max = prod;
+            }
+        }
+        return max;
+    }
+    
+    public static boolean isPalindrome(int num) {
+        String val = Integer.toString(num);
+        String rev = new StringBuffer(val).reverse().toString();
+        if (val.equals(rev))
+            return true;
+        else
+            return false;
+    }
+}
