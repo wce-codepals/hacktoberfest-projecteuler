7e769064324d29e8b8d5386092912a590efa48ef
Hammad Mashkoor <mashkoor.hammad@gmail.com>
2017-10-24 11:59:38 +0530

    Added Solution.js

--- solutions/problem-2/solution.js
+++ solutions/problem-2/solution.js
@@ -1,1 +1,25 @@
-//Add JavaScript solution to problem-2
+function solution()
+ {
+    var fibno = [ 1, 2 ], sum = 0;
+
+ function cal(arr )
+  {
+   return arr[ arr.length - 1 ] + arr[ arr.length - 2 ];
+ }
+
+  while ( fibno[ fibno.length - 1 ] < 4e+6 )
+    {
+      fibno.push( cal(fibno) );
+    }
+
+    fibno.forEach( function(n) 
+     {
+        if ( n % 2 === 0 )
+        {
+            sum += n;
+        }
+    });
+    return sum;
+}
+
+console.log(solution())
