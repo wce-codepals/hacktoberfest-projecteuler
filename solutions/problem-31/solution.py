a412381339e5bc7859bfd3014da34e56f02fe4e6
Ryan Brown <ryanbrown.dev@gmail.com>
2017-10-24 22:11:04 -0500

    python solution for problem-31 coin sum

---
+++ solutions/problem-31/solution.py
@@ -0,0 +1,32 @@
+import sys
+
+"""
+Problem 31
+"""
+
+# Uses dynamic program/optimal substructure solutions
+def findSum(coins, n):
+    coinLen = len(coins)
+    table = [[0 for x in range(coinLen)] for x in range(n+1)]
+
+    for i in range(coinLen):
+        table[0][i] = 1
+
+    for i in range(1, n+1):
+        for j in range(coinLen):
+            # Count of solutions including S[j]
+            x = table[i - coins[j]][j] if i-coins[j] >= 0 else 0
+
+            # Count of solutions excluding S[j]
+            y = table[i][j-1] if j >= 1 else 0
+
+            # total count
+            table[i][j] = x + y
+
+    return table[n][coinLen-1]
+
+targetValue = 200
+coinList = "1 2 5 10 20 50 100 200"
+
+coins = map(int,coinList.strip().split(' '))
+print findSum(coins, targetValue)
