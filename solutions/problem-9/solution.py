3a84585305da2d7734cf8ec941442d6283f917a8
chashikajw <chashikajw007@gmail.com>
2017-10-25 10:31:47 +0530

    set the right logic

--- solutions/problem-9/solution.py
+++ solutions/problem-9/solution.py
@@ -2,10 +2,10 @@ def storeSqareValues(limitNum):
     sqrevalus = []
     val = 1
     i = 1
-    while(val < limitNum-5): #minimum two valuse shuold be 1 and 2(1^2+2^2=5)
-        sqrevalus.append(val)
+    while(i < limitNum): #minimum two valuse shuold be 1 and 2(1^2+2^2=5)
+        sqrevalus.append(i)
         i += 1
-        val = i*i       
+             
         
     return sqrevalus
 
@@ -14,13 +14,16 @@ def findsqares(limitNum):
     lengtharr = len(sqareValues)
     for i in range(lengtharr-1,1,-1):
         for j in range(lengtharr):
-            for k in range(1,lengtharr):
+            for k in range(lengtharr):
                 total = sqareValues[i] + sqareValues[j] + sqareValues[k]
-                if(total == limitNum):
+                if((total == limitNum) and (sqareValues[i]**2 == sqareValues[k]**2 + sqareValues[j]**2 )):
                     print("value 1: "+str(sqareValues[k]))
                     print("value 2: "+str(sqareValues[j]))
                     print("value 3: "+str(sqareValues[i]))
-                    return (sqareValues[i]*sqareValues[j]*sqareValues[k])
+                    print("*******")
+                    return sqareValues[k]*sqareValues[k]*sqareValues[k]
+                
+                    #return (sqareValues[i]*sqareValues[j]*sqareValues[k])
                 elif(total < limitNum):
                     continue
                 else:
