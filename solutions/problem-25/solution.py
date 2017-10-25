b774554f26551681e7ed139a93a61112a082a29b
scotty770 <14sco31@kesgmail.net>
2017-10-25 11:56:13 +0100

    Update solution.py

--- solutions/problem-25/solution.py
+++ solutions/problem-25/solution.py
@@ -4,9 +4,7 @@ num = 0
 while num1 < 1000 or num2 < 1000:
     num1 = num1 + num2
     num += 1
-    if num1 > 1000:
-        break
-    else:
+    if num1 < 1000:
         num2 = num1 + num2
         num += 1
 print (num)
