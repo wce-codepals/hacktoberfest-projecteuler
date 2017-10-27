13a086df6dd0e77e0e2c991f1eb77e6e29fa8be4
unknown <pinalpbhimani123@gmail.com>
2017-10-27 15:24:09 +0530

    Add solution for problem 6 in C++ Fixes #8

--- solutions/problem-6/solution.cpp
+++ solutions/problem-6/solution.cpp
@@ -1,1 +1,13 @@
 //Add CPP solution to problem-6
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int sum_of_squares,square_of_sum,n=10,difference;
+    sum_of_squares=n*(n+1)*(2*n+1)/6;
+    square_of_sum=pow(n*(n+1)/2,2);
+    difference=sum_of_squares-square_of_sum;
+    cout<<"Difference between sum of square and square of sum is "<<difference<<endl;
+    return 0;
+}
