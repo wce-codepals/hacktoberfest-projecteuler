e12c5a04b4f8cc9cae55bf6b17e3d38d81fced9d
kratigya0705 <kratigyarastogi0705@gmail.com>
2017-10-30 00:19:31 +0530

    Solution Added for PROBLEM-3 in java lanaguage

--- solutions/problem-3/solution.java
+++ solutions/problem-3/solution.java
@@ -1,1 +1,15 @@
-//Add Java solution to problem-3
+public class solution {
+	public static void main(String[] args) {
+		long n = 600851475143;
+		long i = 2;
+		while ((i * i) <= n) {
+			if (n%i != 0) {
+				i += 1;
+			} 
+			else {
+				n = (long) Math.floor(n/i);
+			}
+		}
+		System.out.println(n);
+	}
+}
