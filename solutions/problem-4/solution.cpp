76ff7cd72bd221e10dc6207131e1286ea4b1cf8b
ChristianTW <ChristianTW@users.noreply.github.com>
2017-10-29 02:33:22 -0700

    Solution 4 in C++

--- solutions/problem-4/solution.cpp
+++ solutions/problem-4/solution.cpp
@@ -1,1 +1,36 @@
-//Add CPP solution to problem-4
+/*
+ *  Output the largest prime number that is the product of two 3 digit numbers
+ *  Language: C++
+ *  moghya/hacktoberfest-projecteuler
+ *  Problem 4
+ */
+
+#include <iostream>
+
+int reverse(int number);
+
+int main() {
+    int highestPal = 0;
+    for(int i = 100; i <= 999; i++) {
+        for(int k = i; k <= 999; k++)
+        {
+            if(reverse(i*k) == (i*k) && (i*k) > highestPal)
+            {
+                highestPal = i*k;
+            }
+        }
+    }
+    std::cout << highestPal << std::endl;
+}
+
+int reverse(int number)
+{
+    int backwards = 0;
+    
+    while(number > 0)
+    {
+        backwards = 10 * backwards + number % 10;
+        number /= 10;
+    }
+    return backwards;
+}
