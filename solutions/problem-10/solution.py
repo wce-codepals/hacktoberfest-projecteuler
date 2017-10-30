47b908631294a0869d7ad60e3edcd5cc54b999d6
Ja Vouhey <gb.javouhey@gmail.com>
2017-10-29 20:28:25 -0700

    1st

---
+++ solutions/problem-10/solution.py
@@ -0,0 +1,12 @@
+"""
+Find the sum of all the primes below two million.
+
+https://projecteuler.net/problem=10
+"""
+from __future__ import print_function
+
+def solve():
+    return 1
+
+if __name__ == "__main__":
+    print(solve())
