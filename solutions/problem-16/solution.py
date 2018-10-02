3a1b04375ec484bb1de7e76686de9dff086daa92
Atharva Kamat <atharva.makarand15@gmail.com>
2018-10-02 18:38:40 +0530

    Update solution.py
    Added comments

--- solutions/problem-16/solution.py
+++ solutions/problem-16/solution.py
@@ -1,11 +1,11 @@
-def calc(n):
+def calc(n): #function which takes the value 2^1000
     c=0
-    while n!=0:
-        q=n%10
-        n=n//10
-        c=c+q
+    while n!=0: #while the number is not zero
+        q=n%10 #extract digits
+        n=n//10 #remove the digit
+        c=c+q #keep adding the removed digit to get the sum
     return c
 
-r=pow(2,1000)
+r=pow(2,1000) #calculating 2^1000
 w=calc(r)
 print(w)
