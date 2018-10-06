c7942f3ca52371e8b045acd937a5122871007046
root <root@localhost.localdomain>
2018-10-06 21:36:41 +0530

    Added C code

--- solutions/problem-5/solution.c
+++ solutions/problem-5/solution.c
@@ -1,1 +1,30 @@
-/Add C solution to problem-5
+#include <stdio.h>
+
+int main()
+{
+	int num, flag=0;
+	for (int i=1; i<1000000000000; i++)
+	{
+		for (int j=1; j<=20; j++)
+		{
+			if (i%j == 0)
+			{
+				num = i;
+				flag=1;
+			}
+			else
+			{
+				flag=0;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			goto label;
+		}
+	}
+	label:
+		printf("%d\n", num);
+	return 0;
+}
+		
