c47a48895491a122317cea3dda2c4e11c7e73879
root <root@localhost.localdomain>
2018-10-06 20:39:21 +0530

    Made code easier to read

--- solutions/problem-1/solution.c
+++ solutions/problem-1/solution.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-  int i, m1 = 0, m2 = 0, m3 = 0;
-  for (i = 0; i < 1000; i++) 
+  int  m1 = 0, m2 = 0, m3 = 0;
+  for (int i = 0; i < 1000; i++) 
   {
-    if (i % 3 == 0) {
+    if (i % 3 == 0)	//Multiples of 3 
+    {
       m1 += i;
     }
-    if (i % 5 == 0) {
+    if (i % 5 == 0)     //Multiples of 5
+    {
       m2 += i;
     }
-    if (i % 15 == 0) {
+    if (i % 15 == 0)    //Multiples of 15
+    {
       m3 += i;
-  }
+    }
 }
 printf("%d\n", m1 + m2 - m3);
-  return 0;
+return 0;
 }
