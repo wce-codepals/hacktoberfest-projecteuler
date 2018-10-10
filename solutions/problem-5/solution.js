4ada8291509ddc4e9daf128150114bb2f60ea323
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-10-10 20:25:31 +0300

    Adding solution in js to problem 5

--- solutions/problem-5/solution.js
+++ solutions/problem-5/solution.js
@@ -1,1 +1,24 @@
-//Add JavaScript solution to problem-5
+let isDivisble = false;
+const MAX_NUMBER = 21;
+
+function euler_5() {
+	for(let i = 1; !isDivisble; i++) {
+		if (divisbleByNumbers(i)) {
+			isDivisble = true;
+			console.log("IsDivisble " + isDivisble + " number : " + i);
+		}
+	}
+}
+
+function divisbleByNumbers(number) {
+	for(let i = 1; i < MAX_NUMBER; i++) {
+		if (number % i !== 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+euler_5();
+
+//Solution is 232792560
