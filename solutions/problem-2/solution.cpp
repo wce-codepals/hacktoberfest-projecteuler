75b50633d18ad462bf71d9022bd76ad0b784b676
nandanak <36887184+nandanak@users.noreply.github.com>
2018-10-15 19:25:08 +0530

    Solution in cpp

--- solutions/problem-2/solution.cpp
+++ solutions/problem-2/solution.cpp
@@ -1,1 +1,20 @@
 //Add CPP solution to problem-2
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int i,j,k;
+  long long int maxnumber,sum;
+	cout<<"Enter maximum number of fibonacci series ";
+	cin>>maxnumber;
+  i = 0;j = 1;k = 0;sum = 0;
+  while(k <= maxnumber) {
+		if(k % 2 == 0)
+    sum += k;
+    k = i + j;
+    i = j;
+    j = k;
+  }
+  cout<<sum;
+	return 0;
+}
