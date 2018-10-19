73bb4afef1789fc6fe61a42a62ddacc0135f1c41
Adrián Caballero <adrian@epiclabs.io>
2018-10-19 16:13:09 +0200

    Problem 1 solved with typescript

---
+++ solutions/problem-1/solution.ts
@@ -0,0 +1,11 @@
+function sumOfMultiplesOfThreeAndFive(n: number): number {
+  let sum = 0;
+  for(let i = 1; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumOfMultiplesOfThreeAndFive(1000));
