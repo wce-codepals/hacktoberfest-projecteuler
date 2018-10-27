4bc3637a705a3f93181ff99682a5f030e92b5978
rohitpardhi1998 <rohitpardhi90@gmail.com>
2018-10-27 22:29:24 +0530

    Solution Added for PROBLEM-47 in C++

--- solutions/problem-47/47.cpp
+++ solutions/problem-47/47.cpp
@@ -1,39 +1,3 @@
-// ////////////////////////////////////////////////////////
-// # Title
-// Distinct primes factors
-//
-// # URL
-// https://projecteuler.net/problem=47
-// http://euler.stephan-brumme.com/47/
-//
-// # Problem
-// The first two consecutive numbers to have two distinct prime factors are:
-// `14 = 2 * 7`
-// `15 = 3 * 5`
-//
-// The first three consecutive numbers to have three distinct prime factors are:
-// `644 = 2^2 * 7 * 23`
-// `645 = 3 * 5 * 43`
-// `646 = 2 * 17 * 19`
-//
-// Find the first four consecutive integers to have four distinct prime factors each.
-// What is the first of these numbers?
-//
-// # Solved by
-// Stephan Brumme
-// February 2017
-//
-// # Algorithm
-// Most of my prime sieves are based on trial division - this time the Sieve of Eratosthenes is a perfect fit.
-// But instead of "crossing off" composite numbers, I increment a counter:
-// - it's 1 if the number ''i'' itself is the only prime factor (well, then ''i'' is a prime number !)
-// - it's greater than 1 for composite numbers
-//
-// A counter ''currentRun'' is incremented by one as long as the current number has the desired number of distinct prime factors.
-// If the number of prime factors deviated, then ''currentRun'' is reset to zero.
-//
-// If ''currentRun >= consecutive'' then I print the position of the first number, which was ''i - consecutive + 1''.
-
 #include <iostream>
 #include <vector>
 
