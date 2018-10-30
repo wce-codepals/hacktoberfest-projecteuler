409e71523f5ab18cb25557217a2658158cf86fcd
Jovian-Dsouza <43859106+Jovian-Dsouza@users.noreply.github.com>
2018-10-30 09:39:00 +0545

    Update Largest palindrome product .py

--- solutions/problem-4/Largest palindrome product .py
+++ solutions/problem-4/Largest palindrome product .py
@@ -1,6 +1,6 @@
 def largest_palindrome_3digit():
     arr_palindrome=[]
-    for i in range(1,100):            #replace 100 with 1000 and 1 with 100
+    for i in range(1,1000):            
         for j in range(1,100):
             value1=list(str((i*j)))
             if(value1==value1[::-1]):
