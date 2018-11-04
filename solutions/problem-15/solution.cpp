9e3be77abd101b40e42ea80b3d38a70e8b80dd62
Chandana Hari <43808948+Chandananitg@users.noreply.github.com>
2018-11-04 09:39:53 +0530

    aligned loops

--- solutions/problem-15/solution.cpp
+++ solutions/problem-15/solution.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 typedef long long ll;
 ll ModL=pow(10,9)+7;
-int main() {
+int main() 
+{
     ll t,i,j,k,n,m;
     n=501;
     ll a[n+1][n+1];
@@ -10,8 +11,10 @@ int main() {
     {
         for(j=0;j<n;j++)
         {
-            if(i==0||j==0)  a[i][j]=1;
-            else    a[i][j]=(a[i-1][j]+a[i][j-1])%ModL;
+            if(i==0||j==0)  
+                a[i][j]=1;
+            else    
+                a[i][j]=(a[i-1][j]+a[i][j-1])%ModL;
         }
     }
     cin>>t;
