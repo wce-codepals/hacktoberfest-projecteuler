6ae1534b454e353b1c0467fa7e4e88e751c6f2de
Andres Botero <maaboterosi@gmail.com>
2019-10-08 20:38:44 +0200

    Solution Added for PROBLEM 12 in python 3

---
+++ solutions/problem12/solution12.py
@@ -0,0 +1,28 @@
+def divisorsThroughPrimeFactorDecomps (n):
+	factors={}
+	i=2
+	while n>1:
+		if n%i==0:
+			if i in factors.keys():
+				factors[i]+=1
+			else:
+				factors[i]=1
+			n=n/i
+		else:
+			i+=1
+	divisorAmount=1
+	for k,v in factors.items():
+		divisorAmount*=(v+1)
+	#print(factors)
+	return divisorAmount
+	
+
+n=1
+trianglen=1
+while (divisorsThroughPrimeFactorDecomps(trianglen))<500:
+	n+=1
+	trianglen+=n
+	if n%10==0:
+		print(f"{n}, {trianglen}")
+
+print (trianglen)
