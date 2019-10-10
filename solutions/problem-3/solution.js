4ff2de1f2f98aac08d82650317d0725d4f96d51b
aman601 <aman601601@gmail.com>
2019-10-09 22:18:14 -0400

    Update solution.js
    solution 3 in javascript

--- solutions/problem-3/solution.js
+++ solutions/problem-3/solution.js
@@ -1,1 +1,13 @@
-//Add JavaScript solution to problem-3
+var n = 600851475143;
+var x = n;
+var i = 2;
+while(i<x) {
+	while(x % i === 0) {
+		(function(n) {
+			x = n;
+		})
+    (x / i)
+	}
+	i++;
+}
+console.log(x);
