f5aabef204828622364f9d30ad0807a0a5a7b5ec
aman601 <aman601601@gmail.com>
2019-10-09 21:49:36 -0400

    Update solution.js
    Javascript solution to problem 5

--- solutions/problem-5/solution.js
+++ solutions/problem-5/solution.js
@@ -1,1 +1,16 @@
-//Add JavaScript solution to problem-5
+var x = 1;
+var True = true;
+while(True) {
+  var works = true;
+  for(i = 1; i < 21; i++){
+    if(x % i != 0) {
+      works = false;
+      x += 1;
+      break;
+    }
+  }
+  if(works) {
+    console.log(x);
+    break;
+  }
+}
