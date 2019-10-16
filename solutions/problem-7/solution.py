ec224c25d6ccea588fa0da83b98b101596a9bcd5
Aakash Kumar <akashkmr27089@gmail.com>
2019-10-16 23:27:49 +0530

    solution.py
    Python implementation for finding 100001st prime

---
+++ solutions/problem-7/solution.py
@@ -0,0 +1,17 @@
+import math
+
+count = 1
+nos =2
+val = 1
+
+while(val!=10001):
+    count = count +1
+    if(count%2 == 1):
+        flag = True
+        for i in range(2, int(math.sqrt(count))+1):
+            if(count%i == 0):
+                flag = False
+        if flag == True:
+            val += 1
+
+print(count)
