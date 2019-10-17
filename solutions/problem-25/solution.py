6dc32c8d94f1e4768dbcdf29a2c83510a9cfe4d5
Aakash Kumar <akashkmr27089@gmail.com>
2019-10-17 10:48:57 +0530

    Solution Added for PROBLEM-25 in Python language

--- solutions/problem-25/solution.py
+++ solutions/problem-25/solution.py
@@ -1,8 +1,8 @@
-a = [1,1]
+a = [1,1] #inital fibbonachi number 
 
-def find(data):
+def find(data):     #finds the fibbonachi number of given index
     if(data <= a.__len__()):
-        return a[data-1]
+        return a[data-1]        #if number exist then return else its appended sequentially
     else:
         while(a.__len__()!=data):
             a.append(a[-2]+a[-1])
@@ -12,6 +12,6 @@ def find(data):
 val = 4000
 g = 0
 while(g!=1000):
-    g = str(find(val)).__len__()
+    g = str(find(val)).__len__()    #finding number of digits
     val += 1
-print(val-1)
+print(val-1)    #final output
