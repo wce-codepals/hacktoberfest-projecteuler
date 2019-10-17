a2dfa4ef3757fcde0c31757eaf15640552ee3763
Aakash Kumar <akashkmr27089@gmail.com>
2019-10-17 10:58:51 +0530

    Solution Added for PROBLEM-7 in Python language

--- solutions/problem-7/solution.py
+++ solutions/problem-7/solution.py
@@ -4,11 +4,11 @@ count = 1
 nos =2
 val = 1
 
-while(val!=10001):
+while(val!=10001):  
     count = count +1
     if(count%2 == 1):
         flag = True
-        for i in range(2, int(math.sqrt(count))+1):
+        for i in range(2, int(math.sqrt(count))+1): #check for the divisibility
             if(count%i == 0):
                 flag = False
         if flag == True:
