c8d473b78cf4c2a0fad8c820ab1da39320555a7f
pooja2299 <44069818+pooja2299@users.noreply.github.com>
2019-10-20 13:06:48 +0530

    Update solution.c

--- solutions/problem-16/solution.c
+++ solutions/problem-16/solution.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include<math.h>
+
 
 int main()
 {
-    int t,i=0,n,sum;
-    long p;
-    printf("\nEnter no. of test cases: ");
-    scanf("%d",&t);
-    while(i<t)
-    {
-        printf("\nEnter power : ");
-        scanf("%d",&n);
-        p=pow(2,n);
-        sum=0;
-        while(p)
-        {
-            sum+=p%10;
-            p/=10;
-        }
-        printf("\nSum:%d",sum);
-        i++;
-    }
-
-    return 0;
+    int a[10000]={0};
+	int m=1,i,j;
+	int carry=0;
+	a[0]=1;
+	long long int sum=0;
+	for(i=1;i<=1000;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			int x=a[j]*2+carry;
+			a[j]=x%10;
+			carry=x/10;
+		}
+		while(carry!=0)
+		{
+			a[m++]=carry%10;
+			carry/=10;
+		}
+	}
+	for(i=m-1;i>=0;i--)
+	sum+=a[i];
+	printf("%lld\n",sum);
+	return 0;
 }
