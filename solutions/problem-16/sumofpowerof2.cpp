98cf570eb4e0d78ef46909bcd3714f1cb05db159
pooja2299 <44069818+pooja2299@users.noreply.github.com>
2019-10-20 12:54:30 +0530

    Update sumofpowerof2.cpp

--- solutions/problem-16/sumofpowerof2.cpp
+++ solutions/problem-16/sumofpowerof2.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-
-int main()
-{
-    int t,i=0,n,sum;
-    long p;
-    cout<<"\nEnter no. of test cases: ";
-    cin>>t;
-    while(i<t)
-    {
-        cout<<"\nEnter power : ";
-        cin>>n;
-        p=pow(2,n);
-        sum=0;
-        while(p)
-        {
-            sum+=p%10;
-            p/=10;
-        }
-        cout<<"\nSum= "<<sum;
-        i++;
-    }
-
-    return 0;
+ 
+int main() {
+	
+	int a[10000]={0};
+	int m=1;
+	int carry=0;
+	a[0]=1;
+	long long int sum=0;
+	for(int i=1;i<=1000;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			int x=a[j]*2+carry;
+			a[j]=x%10;
+			carry=x/10;
+		}
+		while(carry!=0)
+		{
+			a[m++]=carry%10;
+			carry/=10;
+		}
+	}
+	for(int i=m-1;i>=0;i--)
+	sum+=a[i];
+	cout<<sum<<"\n";
+	return 0;
 }
