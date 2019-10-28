a66480ef30e2a55fdaacf9e5f1b806ead214e713
Sujan Chegu <sujanchg@gmail.com>
2019-10-28 13:04:07 +0530

    Better solution for Problem-8

--- solutions/problem-8/solution.c
+++ solutions/problem-8/solution.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main() 
+int main()
 {
-	int i,t,n,k,j,l;char s[1001];
-	long long p,max;
-	scanf("%d",&t);
-	for(i=0;i<t;i++) 
+	int a[1000];
+	int largestprod = 1;
+	int prod = 1;
+	printf("Enter all the digits of the number seperated by spaces\n");
+	for (int i = 0;i< 10; i++)
 	{
-		max=0;
-		scanf("%d %d %s",&n,&k,&s);
-	 	for(j=0;j<=n-k;j++)
-	      	{
-	     		p=1;
-		     	for(l=j;l<j+k;l++)
-			{
-		 		p*=(long long)(s[l]-48);
-	     		}
-	     		if(p>max)
-		 		max=p;
-	 	}
-            	printf("%Ld\n",max);
-        }
-    return 0;
-}
+		scanf("%d", &a[i]);
+		if (i > 3)
+		{
+			prod = a[i] * a[i - 1] * a[i - 2] * a[i - 3];
+		}
+		else
+		{
+			prod = prod * a[i];
+			largestprod = prod;
+		}
+		
 
+		if (prod > largestprod)
+		{
+			largestprod = prod;
+		}
+	}
+	printf("%d",largestprod);
+	return 0;
+}
