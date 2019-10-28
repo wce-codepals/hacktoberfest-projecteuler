8451a091e6ffc513f02315b0a500faec1b0490ea
nandakrishna75 <nandakrishna75@gmail.com>
2019-10-28 20:42:16 +0530

    Added solution to problem 19

---
+++ solutions/problem-19/solution.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+# Solve using brute force.
+def solve():
+	l = [1 for y in range(1901, 2001) for m in range(1, 13) if datetime.date(y, m, 1).weekday() == 6]
+	return sum(l)
+
+
+if __name__ == "__main__":
+	sol = solve()
+	print(solve())
