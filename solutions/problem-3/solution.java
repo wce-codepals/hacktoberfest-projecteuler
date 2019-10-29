beb8c3a30e576595864d2714a732ee50efc7a563
dragmarim <50841715+dragmarim@users.noreply.github.com>
2019-10-28 21:21:36 -0400

    Update solution.java

--- solutions/problem-3/solution.java
+++ solutions/problem-3/solution.java
@@ -1,1 +1,24 @@
-//Add Java solution to problem-3
+public class LargestPrimeFactor {
+
+	public static void main(String[] args) {
+		long num = 600851475143L;
+		long currentPrimeNumber = 0;
+		for (long j = 2; j < num/2; j++) {
+			if (num % j == 0) {
+				boolean isPrime = true;
+				for (long i = 2; i < j/2; i++) {
+					if (j % i == 0) {
+						isPrime = false;
+					}
+				}
+				if (isPrime) {
+					currentPrimeNumber = j;
+					System.out.println(j);
+				}
+			}
+			//System.out.println(num);
+		}
+		System.out.println(currentPrimeNumber + "end");
+	}
+
+}
