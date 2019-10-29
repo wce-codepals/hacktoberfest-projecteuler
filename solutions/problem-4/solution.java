028865c9d307322b671cff4573aca57a1a74d62d
dragmarim <50841715+dragmarim@users.noreply.github.com>
2019-10-28 21:22:23 -0400

    Update solution.java

--- solutions/problem-4/solution.java
+++ solutions/problem-4/solution.java
@@ -1,1 +1,27 @@
-//Add Java solution to problem-4
+public class Palindromic {
+
+	public static void main(String[] args) {
+		int firstNum = 0;
+		int secondNum = 0;
+		int solution = 0;
+		for (secondNum = 100; secondNum < 1000; secondNum++) {
+			for (firstNum = 100; firstNum < 1000; firstNum++) {
+				boolean isPalindrome = true;
+				String s = Integer.toString(firstNum * secondNum);
+				while (s.length() > 1 && isPalindrome) {
+					if (s.charAt(0) == s.charAt(s.length()-1)) {
+						System.out.println("FirstNum: " + firstNum + " SecondNum: " + secondNum + " S: " + s);
+						s = s.substring(1, s.length()-1);
+					} else {
+						isPalindrome = false;
+					}
+				}
+				if (firstNum * secondNum > solution && isPalindrome) {
+					solution = firstNum * secondNum;
+				}
+			}
+		}
+		System.out.println(solution);
+	}
+
+}
