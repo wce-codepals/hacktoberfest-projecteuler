85eb6ba9834370b3725071ee3f992d02f6400644
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2020-10-06 00:54:27 +0530

    Added solution.c for problem 6

---
+++ solutions/problem-6/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int n=100;
+	long long int sum=0,square_sum=0;
+	for(int i=1;i<=n;i++)
+	{
+		sum+=i;
+		square_sum+=i*i;
+	}
+	sum*=sum;
+	printf("%lld\n",sum-square_sum);
+	return 0;
+}
