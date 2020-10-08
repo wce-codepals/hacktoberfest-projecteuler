47fae1e6f73fcdbce957aa9562613f119e900c9f
DevBoy69 <61925552+DevBoy69@users.noreply.github.com>
2020-10-08 06:10:58 +0530

    Solution Added for PROBLEM-73 in Python

---
+++ solutions/problem-73/solution.py
@@ -0,0 +1,16 @@
+def compute():
+	ans = 0
+	stack = [(1, 3, 1, 2)]
+	while len(stack) > 0:
+		leftn, leftd, rightn, rightd = stack.pop()
+		d = leftd + rightd
+		if d <= 12000:
+			n = leftn + rightn
+			ans += 1
+			stack.append((n, d, rightn, rightd))
+			stack.append((leftn, leftd, n, d))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
