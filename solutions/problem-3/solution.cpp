a3ea15bdebbee0aa4498c18e3b5cbd6deaa89015
Souvik Mahato <souvikmahato2279@gmail.com>
2020-10-09 19:12:05 +0530

    Solution added for problem-3 in cpp

--- solutions/problem-3/solution.cpp
+++ solutions/problem-3/solution.cpp
@@ -1,1 +1,24 @@
-//Add CPP solution to problem-3
+#include<iostream>
+using namespace std;
+
+long long int highest_primefactor(long long int n){
+    long long int f = 2;
+    while(f <= n){
+        if(n % f == 0){
+            n /= f;
+        }
+        else{
+            f++;
+        }
+    }
+    return f;
+}
+
+int main(){
+    long long int n;
+    cout<<"Enter the number :- ";
+    cin >> n;
+    long long int ans = highest_primefactor(n);
+    cout<<"Highest prime factor of "<<n<<" is = "<<ans<<endl;
+    return 0;
+}
